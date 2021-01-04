ed4820d2211a5a6d0632edccb656654862e49f9a
Grant Dempsey <CryptoDemps@gmail.com>
2021-01-04 04:25:32 -0700

    Add api call welcome and private methods

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  before_action :ensure_logged_in, only: [:private]
+
+  def welcome
+
+  end
+
+  def private
+    username = current_user.name
+
+    conn = Faraday.new(url: "https://api.github.com")
+
+    response = conn.get("/user/repos?access_token=cce71ae4b54fc63ab10e055447c27c6156d4ae52")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    @repos = json
+  end
+end
